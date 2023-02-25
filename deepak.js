b5473c2b63666aace3b76c47c3ef064f9bb96229
vhald <vaibhavgsh@gmail.com>
2023-02-25 13:16:21 +0530

    deepak mama

---
+++ deepak.js
@@ -0,0 +1,21 @@
+// function deepak(ladki1, ladki2) {
+//     if (!ladki1) {
+//         console.log("go to ladki 2")
+//     } else {
+//         console.log("gaand maraao", ladki2)
+//     }
+// }
+
+// deepak(null, "priya")
+
+// truthy
+// agar koi value h -> undefined nhi h, lekin wo 0, null, NaN, string, number, object, boolean -> true,
+
+// falsy - false, undefined,
+// -
+
+// const deepakMama = !true;
+// console.log(deepakMama, "i am deepakmama")
+
+// const a = true;
+
